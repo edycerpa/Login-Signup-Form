0a5886681df368617dbf9330e4468b62f953b3b6
Eddy Cerpa <82135358+edycerpa@users.noreply.github.com>
2022-07-18 09:35:28 -0500

    .

---
+++ app.js
@@ -0,0 +1,12 @@
+function signupBtn (){
+    document.querySelector('body').classList.toggle('active'),
+    document.querySelector('.box-form').classList.toggle('active'),
+    document.querySelector('.login-form').classList.toggle('active'),
+    document.querySelector('.signup-form').classList.toggle('active')
+}
+function loginBtn (){
+    document.querySelector('body').classList.toggle('active'),
+    document.querySelector('.box-form').classList.toggle('active'),
+    document.querySelector('.login-form').classList.toggle('active'),
+    document.querySelector('.signup-form').classList.toggle('active')
+}
